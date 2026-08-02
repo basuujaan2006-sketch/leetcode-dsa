fded02561191383f31845f5758989f6c3d314bd8
Ujaan Basu <basuujaan2006@gmail.com>
2026-08-02 11:46:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int temp = x;
+        long rev = 0;
+        while (x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return temp == rev;
+    }
+};
