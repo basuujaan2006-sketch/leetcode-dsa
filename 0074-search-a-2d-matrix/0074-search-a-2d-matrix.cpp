9bd6ac844601861fb323417b47f03b08ae78f239
Ujaan Basu <basuujaan2006@gmail.com>
2026-08-14 00:02:26 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.49%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool searchinrow(vector<vector<int>>& mat, int target, int midrow){
+        int n = mat[midrow].size();
+        int st = 0, end = n-1;
+        while(st <= end){
+            int mid = st + (end - st)/ 2;
+            if (target == mat[midrow][mid]){
+                return true;
+            }
+            else if(target > mat[midrow][mid]){
+                st = mid +1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;  
+    }
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size(), n = mat[0].size();
+        int strow = 0, endrow = m-1;
+        while(strow <= endrow){
+            int midrow = strow + (endrow-strow)/2;
+            if(target >= mat[midrow][0] && target <= mat[midrow][n-1]){
+                return searchinrow(mat, target, midrow);
+            }
+            else if(target >= mat[midrow][n-1]){
+                strow = midrow + 1; 
+            }
+            else{
+                endrow = midrow - 1;
+            }
+        }
+        return false;
+    }
+};
